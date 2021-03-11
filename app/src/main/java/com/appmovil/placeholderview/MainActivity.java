972ef414e83a3949883d57103a0c767733e09a89
KevinCheves <kevin.cheves2016@uteq.edu.ec>
2021-03-11 17:21:17 -0500

    commit 1

--- app/src/main/java/com/appmovil/placeholderview/MainActivity.java
+++ app/src/main/java/com/appmovil/placeholderview/MainActivity.java
@@ -4,7 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.appmovil.placeholderview.WebService.Asynchtask;
+import com.appmovil.placeholderview.WebService.WebService;
+
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements Asynchtask {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     GalleryImage Adaptador = new GalleryImage(this,"" );
- 
+
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws= new WebService("https://revistas.uteq.edu.ec/ws/journals.php",
+               datos, MainActivity.this,MainActivity.this);
+        ws.execute("GET");
+    }
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+        
     }
 }
